c94d5c5b0afb52774d17dbbe73046e2e7487c0d2
Mjakot <hlebletjaslomaju@gmail.com>
2024-04-19 15:22:15 +0300

    Debugger finally runs

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,7 +3,7 @@ require 'utils'
 nmap('<Esc>', '<cmd>nohlsearch<CR>', 'Clear highlight on search')
 nmap('<C-C>', '<cmd>nohlsearch<CR>', 'Clear highlight on search')
 
-nmap(']d', vim.diagnostic.goto_next, 'Go to next [D]iagnostic message')
+nmap('[d', vim.diagnostic.goto_next, 'Go to next [D]iagnostic message')
 nmap(']d', vim.diagnostic.goto_prev, 'Go to next [D]iagnostic message')
 nmap('<leader>e', vim.diagnostic.open_float, 'Show diagnostic [E]rror message')
 nmap('<leader>q', vim.diagnostic.setloclist, 'Open diagnostic [Q]uickfix list')
@@ -15,6 +15,3 @@ nmap('<C-k>', '<C-w><C-k>', 'Move focus to the upper window')
 
 nmap('<C-d>', '<C-d>zz', 'Scroll [D]own half page and center cursor')
 nmap('<C-u>', '<C-u>zz', 'Scroll [U]p half page and center cursor')
-
-nmap('{', '}zz', 'Scroll down one paragraph and center cursor')
-nmap('}', '{zz', 'Scroll up one paragraph and center cursor')
--- lua/mjakot/plugins/debug.lua
+++ lua/mjakot/plugins/debug.lua
@@ -20,30 +20,92 @@ local M = {
       },
     }
 
-    nmap('<F5>', dap.continue, 'Debug: Start/Continue')
-    nmap('<F10>', dap.step_into, 'Debug: Step Into')
-    nmap('<F11>', dap.step_over, 'Debug: Step Over')
-    nmap('<F12>', dap.step_out, 'Debug: Step Out')
-    nmap('<leader>b', dap.toggle_breakpoint, 'Debug: Toggle Breakpoint')
-
-    dapui.setup {
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '❚❚',
-          play = '▶',
-          step_into = '↓',
-          step_over = '→',
-          step_out = '↑',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '■',
-          disconnect = 'x',
+    dap.cpp = {
+      codelldb = {
+        {
+          type = 'cpp',
+          name = 'Debug',
+          request = 'launch',
+          program = '${file}',
         },
       },
     }
 
-    nmap('<F7>', dapui.toggle, 'Debug: See last session result')
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+	-- stylua: ignore start
+        command = vim.fn.stdpath('data') .. '\\mason\\bin\\codelldb.cmd',
+        -- stylua: ignore end
+        args = { '--port', '${port}' },
+        detached = false,
+      },
+    }
+
+    nmap('<F5>', dap.continue, 'Debug: [F5] Start/Continue')
+    nmap('<F10>', dap.step_into, 'Debug: [F10] Step Into')
+    nmap('<F11>', dap.step_over, 'Debug: [F11] Step Over')
+    nmap('<F12>', dap.step_out, 'Debug: [F12] Step Out')
+    nmap('<leader>b', dap.toggle_breakpoint, 'Debug: Toggle [B]reakpoint')
+    nmap('<leader>cb', function()
+      dap.clear_breakpoints()
+    end, 'Debug: [C]lear [B]reakpoints')
+    nmap('<leader>x', function()
+      dap.clear_breakpoints()
+      dap.terminate()
+    end, 'Debug: [X] Terminate Debugging and Clear Breakpoints')
+
+    dapui.setup {
+      icons = {
+        expanded = '▾',
+        collapsed = '▸',
+        current_frame = '*',
+      },
+      mappings = {
+        open = 'o',
+        remove = 'd',
+        edit = 'e',
+        repl = 'r',
+        toggle = 't',
+      },
+      expand_lines = true,
+      layouts = {
+        {
+          elements = {
+            'scopes',
+          },
+          size = 0.3,
+          position = 'left',
+        },
+        {
+          elements = {
+            'repl',
+            'breakpoints',
+          },
+          size = 0.3,
+          position = 'bottom',
+        },
+      },
+      floating = {
+        max_height = nil,
+        max_width = nil,
+        border = 'single',
+        mappings = {
+          close = {
+            'q',
+            '<Esc>',
+          },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        max_type_length = nil,
+        indent = 1,
+      },
+    }
+
+    nmap('<F7>', dapui.toggle, 'Debug: [F7] See last session result')
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
